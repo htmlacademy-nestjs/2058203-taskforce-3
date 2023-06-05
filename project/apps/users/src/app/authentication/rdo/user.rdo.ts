77111e5cd56711d06c6ab03af8e5fa2a500eff86
Yuliya8903 <prigodina.yuliya@gmail.com>
2023-06-05 22:56:29 +0700

    Создаём проекты, готовим REST

---
+++ project/apps/users/src/app/authentication/rdo/user.rdo.ts
@@ -0,0 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+
+export class UserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '22'
+  })
+  @Expose({name: '_id'})
+  public id: string;
+
+  @ApiProperty({
+    description: 'User avatar path',
+    example: '/images/user.png'
+  })
+  @Expose()
+  public avatar: string;
+
+  @ApiProperty({
+    description: 'User date birth (ISO format)',
+    example: '2002-02-22'
+  })
+  @Expose()
+  public dateBirth: string;
+
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.local'
+  })
+  @Expose()
+  public email: string;
+
+  @ApiProperty({
+    description: 'User first name',
+    example: 'Keks'
+  })
+  @Expose()
+  public firstname: string;
+
+  @ApiProperty({
+    description: 'User last name',
+    example: 'Ivanov'
+  })
+  @Expose()
+  public lastname: string;
+}
